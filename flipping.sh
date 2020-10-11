9fe24800522558396de3908bd8bc1b7190364a69
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-12 01:00:14 +0530

    added:singlets percentage

--- flipping.sh
+++ flipping.sh
@@ -1,12 +1,34 @@
 #!/bin/bash -x
- for((i=0;i<2;i++))
+read -p "number of times coin to be flipped" num
+head=1
+tail=0
+headcount=0
+tailcount=0
+for ((i=0;i<$num;i++))
 do
-	randomCheck=$((RANDOM%2))
-
+	randomcheck=$((RANDOM%2))
+	case $randomcheck in
+ 
+	$head)
+		((headcount++))
+		;;
+	$tail)
+		((tailcount++))	
+		;;
+esac
 done
-if [ $randomCheck -eq 0 ]
-then
-	echo "flipped coin is head"
-else
-	echo "flipped coin is tail "
-fi
+echo "count of head is " $headcount
+echo "count of tail is " $tailcount
+declare -A dictionary
+dictionary[((head))]=$headcount
+dictionary[((tail))]=$tailcount
+echo ${dictionary[@]}
+
+resulthead=$(($headcount*100))
+resultheadper=$(($resulthead/$num))
+
+resulttail=$(($tailcount*100))
+resulttailper=$(($resulttail/$num))
+
+echo "percentage of head " $resultheadper
+echo "percentage of tail " $resulttailper
